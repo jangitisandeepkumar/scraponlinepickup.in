7516eb0ef2f02e2c2e6d39450dce44d46d0fec8b
jangitisandeepkumar <sandeepkumarjangiti37@gmail.com>
2026-03-26 20:14:04 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -15,10 +15,10 @@ import {
 
 // Firebase Config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCA-v0X9OzsToVlNHYBd1WRrOi01CWotVY",
   authDomain: "login-51a65.firebaseapp.com",
   projectId: "login-51a65",
-  storageBucket: "login-51a65.appspot.com",
+  storageBucket: "login-51a65.firebasestorage.app",
   messagingSenderId: "1051725155986",
   appId: "1:1051725155986:web:31ab5d15f993e9eee0e04b"
 };
